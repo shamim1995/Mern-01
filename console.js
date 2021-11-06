9764c35a1631acbd1845911971ef5d9a1561cd73
Hamidul Islam shamim <mdhamidul2714@gmail.com>
2021-11-07 00:53:27 +0600

    final

---
+++ console.js
@@ -0,0 +1,12 @@
+console.log('My Name Is Hamidul Islam Shamim');
+
+console.dir("I am a Front end and wordpress specialist");
+
+// This is template literale
+let fName='shamim ';
+let lName='mia'
+console.log(`My name is ${fName+lName}`);
+
+console.log(`This is Hamidul Islam Shamim.
+I am a Developer,
+I live at Pathakata in sherpur District`);
